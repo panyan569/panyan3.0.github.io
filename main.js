2cba51dd722a75e85a91b12462e844c7a8270ae2
panyan569 <3364512587@qq.com>
2024-09-19 00:14:32 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
+
+const images = ['图片1.jpg', `图片2.jpg`, `图片3.jpg`, `图片4.jpg`, `图片5.jpg`];
+const alts = {
+  '图片1.jpg' : 'Stone dog',
+  '图片2.jpg' : 'Flower tree',
+  '图片3.jpg' : 'Elephant sign',
+  '图片4.jpg' : 'Milky tea',
+  '图片5.jpg' : 'Elephant doll'
+}
+
+/* Looping through images */
+
+for (const image of images) {
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', `images/${image}`);
+  newImage.setAttribute('alt', alts[image]);
+  thumbBar.appendChild(newImage);
+  newImage.addEventListener('click', e => {
+    displayedImage.src = e.target.src;
+    displayedImage.alt = e.target.alt;
+  });
+}
+
+/* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', () => {
+  const btnClass = btn.getAttribute('class');
+  if (btnClass === 'dark') {
+    btn.setAttribute('class','light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } else {
+    btn.setAttribute('class','dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
+});
